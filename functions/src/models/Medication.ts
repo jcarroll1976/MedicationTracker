67a3ce81f982aa4e5a89a5991d8ebdac9bed0cc1
Joshua Carroll <carrolljosh1976@gmail.com>
2024-04-14 12:30:40 -0400

    added MedicationList, Medication, and DosageLog interfaces for backend

--- functions/src/models/Medication.ts
+++ functions/src/models/Medication.ts
@@ -1,7 +1,22 @@
+import { ObjectId } from "mongodb";
+
 export interface MedicationList {
     allMedications: Medication[]
 }
 
 export interface Medication {
-    
+    id?: ObjectId;
+    name: string;
+    dosage: number;
+    frequency: string;
+    refillDate: string;
+    instructions: string;
+    sideEffects: string;
+}
+
+export interface DosageLog {
+    id?: string;
+    medicationId: string;
+    date: string;
+    time: string;
 }
